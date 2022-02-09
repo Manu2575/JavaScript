e876ac9a1e87abeea7266869b4d05d2a9beac337
Manu2575 <98060632+Manu2575@users.noreply.github.com>
2022-02-09 02:15:00 -0300

    Atualização da aula12

--- aula12modelo/script.js
+++ aula12modelo/script.js
@@ -3,21 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 6) {
-        //Bom Madrugada!
+        msg.innerHTML = `Agora são ${hora} horas. Boa Madrugada!`
         img.src = 'madrugada.foto.jpg'
         document.body.style.background = 'black'
     } else if (hora >= 6 && hora < 12) {
-        //Bom Dia!
+        msg.innerHTML = `Agora são ${hora} horas. Bom Dia!`
         img.src = 'manha.foto.jpg'
-        document.body.style.background = '#e2cd9f'
+        document.body.style.background = '#e2cf4f'
     } else if (hora >= 12 && hora <= 18) {
-        //Boa Tarde!
+        msg.innerHTML = `Agora são ${hora} horas. Boa Tarde!`
         img.src = 'tarde.foto.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        //Boa Noite!
+        msg.innerHTML = `Agora são ${hora} horas. Boa Noite!`
         img.src = 'noite.foto.jpg'
         document.body.style.background = '#515154'
     }

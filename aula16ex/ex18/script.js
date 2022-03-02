d313584b9de07fa5a9cfe1561a413b6050a08014
Manu2575 <98060632+Manu2575@users.noreply.github.com>
2022-03-02 12:00:28 -0300

    Atualizando a aula16 exercício18

---
+++ aula16ex/ex18/script.js
@@ -0,0 +1,28 @@
+let num = document.querySelector('unput#fnum')
+let lista = document.querySelector('selsct#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, 1) {
+    if (1.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        window.alert('valor inválido ou já encontrado na lista.')
+    }
+}
